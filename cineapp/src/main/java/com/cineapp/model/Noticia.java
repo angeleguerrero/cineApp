c635b84733207ff01a06078bdec894377d7c2e87
HELLAS <HELLAS@HELLAS>
2020-09-06 22:41:34 -0400

    Update Class to JPA

--- cineapp/src/main/java/com/cineapp/model/Noticia.java
+++ cineapp/src/main/java/com/cineapp/model/Noticia.java
@@ -2,8 +2,22 @@ package com.cineapp.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="noticias")
 public class Noticia {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	
+	
+	
 	private Integer id;
 	private String titulo;
 	private Date fecha;
